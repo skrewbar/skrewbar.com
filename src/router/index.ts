b80ef131d4511bc27423ff1e7bf6899c2ac547da
skuru <skurua1244@gmail.com>
2025-02-19 21:39:30 +0900

    🔨 dataview router 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,11 @@ const router = createRouter({
       component: () => import('../views/LinksView.vue'),
     },
     {
+      path: '/data',
+      name: 'data',
+      component: () => import('../views/DataView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
