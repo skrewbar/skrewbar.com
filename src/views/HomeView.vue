701f081fa5cf23db524889293f75573ceceb798e
skuru <skurua1244@gmail.com>
2025-02-19 14:33:43 +0900

    🚧 홈 뷰 마인크래프트 섹션 완성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,14 +9,41 @@
      -->
     <img class="avatar" src="https://avatars.githubusercontent.com/u/66629525?v=4" />
     <h1 class="title">안녕하세요, 코딩을 좋아하는 스쿠루입니다!</h1>
-    <h1><span class="rainbow">만들고 싶은 걸</span> 만들고 있습니다.</h1>
+    <h1 class="title"><span class="rainbow">만들고 싶은 걸</span> 만들고 있습니다.</h1>
+
+    <h1 class="section-title">마인크래프트/희망 서버</h1>
+    <p>희망 서버라는 자그만 마인크래프트 서버에서 여러 플러그인 개발을 담당했습니다.</p>
+    <section id="mailplugin">
+      <h1 class="section-title">메일 플러그인</h1>
+      <figure>
+        <img src="../assets/images/sendmail_screenshot.png" width="50%" />
+        <img src="../assets/images/receivemail_screenshot.png" width="50%" />
+        <figcaption>메일 플러그인</figcaption>
+      </figure>
+      <p>제가 처음으로 완성한 프로젝트입니다.</p>
+      <p>간단한 명령어와 GUI로 편하게 사용할 수 있도록 개발했습니다.</p>
+      <p>처음 제대로 해보는 개발이라 힘들었지만 그만큼 많이 배우게 된 계기였습니다.</p>
+    </section>
+    <section id="efx">
+      <h1 class="section-title">각종 무기와 이펙트</h1>
+      <p>잡다한 무기들의 능력과 이펙트를 개발했습니다.</p>
+      <p>아래 영상의 원형 이펙트를 만드는 것이 가장 힘들었습니다.</p>
+      <iframe
+        class="fragment fade-in"
+        src="https://www.youtube.com/embed/CnkoK96JHF8?si=IvlcfHFjY95-5da8"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </section>
   </main>
 </template>
 
 <style scoped>
 .avatar {
   border-radius: 50%;
-  width: 12rem;
+  width: 20%;
 }
 
 .rainbow {
@@ -35,5 +62,23 @@
   );
   background-clip: text;
   color: transparent;
+}
+
+p {
+  font-size: 2rem;
+}
+
+figcaption {
+  font-size: 1.5rem;
+}
+
+iframe {
+  width: 50vw;
+  height: 50vh;
+}
+
+.media {
+  min-width: 10rem;
+  min-height: 10rem;
 }
 </style>
