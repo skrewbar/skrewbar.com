de5291efd29ba0c56c6bb88528575e191b6fc644
skuru <skurua1244@gmail.com>
2025-02-19 21:39:47 +0900

    🚧 dataview wip

---
+++ src/views/DataView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="wip">
+      <h1 class="title">Work In Progress...</h1>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.wip {
+  width: 100%;
+  height: 50vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
