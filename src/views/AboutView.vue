161abc0364502a1cbc755954e7164a93d78b3755
skuru <skurua1244@gmail.com>
2025-02-19 16:01:40 +0900

    ✨ About 뷰

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,70 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <section id="language">
+      <h1 class="section-title">언어</h1>
+      <img
+        class="icon"
+        src="https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
+        alt="python"
+      />
+      <img
+        class="icon"
+        src="https://i.namu.wiki/i/MuCO_ocla-FyadGnRZytkRLggQOcqxv_hXNjN7aYXDOPivIChJNdiRXp6vwSXbM6GcUL3pVTL-5U5TKQ0f1YhA.svg"
+        alt="java"
+      />
+      <img
+        class="icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/911px-ISO_C%2B%2B_Logo.svg.png"
+        alt="cpp"
+      />
+      <img
+        class="icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png"
+        alt="html5"
+      />
+      <img
+        class="icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png"
+        alt="css3"
+      />
+      <img
+        class="icon"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png"
+        alt="javascript"
+      />
+    </section>
+    <section id="solvedac">
+      <h1 class="section-title">solved.ac</h1>
+      <a href="https://solved.ac/skuru" rel="nofollow"
+        ><img
+          src="https://camo.githubusercontent.com/1fd82ffa032e27837f60864c5ad1ae5fb93217f283f17baf030b95d8cf27c671/687474703a2f2f6d617a617373756d6e6964612e7774662f6170692f76322f67656e65726174655f62616467653f626f6a3d736b757275"
+          alt="solved.ac 프로필"
+          data-canonical-src="http://mazassumnida.wtf/api/v2/generate_badge?boj=skuru"
+          style="max-width: 100%" /></a
+      ><a
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+        href="https://camo.githubusercontent.com/672d67ca0b32cf200ad220e04138f3a0c2c9fa7f87e346734ed6a6923773e06a/687474703a2f2f6d617a616e64692e6865726f6b756170702e636f6d2f6170693f68616e646c653d736b757275267468656d653d7761726d"
+        ><img
+          src="https://camo.githubusercontent.com/672d67ca0b32cf200ad220e04138f3a0c2c9fa7f87e346734ed6a6923773e06a/687474703a2f2f6d617a616e64692e6865726f6b756170702e636f6d2f6170693f68616e646c653d736b757275267468656d653d7761726d"
+          alt="mazandi profile"
+          data-canonical-src="http://mazandi.herokuapp.com/api?handle=skuru&amp;theme=warm"
+          style="max-width: 100%"
+      /></a>
+    </section>
+  </main>
 </template>
 
-<style></style>
+<style>
+.icon {
+  width: 3rem;
+  margin: 0.5rem;
+}
+
+a {
+  padding: 0.5rem;
+}
+a:nth-child(2){
+  padding-left: 0;
+}
+</style>
