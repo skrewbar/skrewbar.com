dfb88eac06f924aa134785e7bc276528cb13fe01
skuru <skurua1244@gmail.com>
2025-02-21 00:58:26 +0900

    🔥 잘못 올린 svg 제거

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,6 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
-
-  <svg id="sunmoon" width="200" height="200">
-    <defs>
-      <mask id="hole">
-        <rect width="100%" height="100%" fill="white" />
-        <circle id="overlay" r="80" cx="230" cy="-30" fill="black" />
-      </mask>
-
-      <filter id="blur">
-        <feDropShadow dx="0" dy="0" stdDeviation="10" flood-color="gold" />
-      </filter>
-    </defs>
-
-    <g filter="url(#blur)">
-      <circle fill="gold" id="donut" r="80" cx="100" cy="100" mask="url(#hole)" />
-    </g>
-  </svg>
 </template>
 
 <style scoped>
